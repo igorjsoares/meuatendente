7b1b9d55a21218b2fa216933580de0997be254b0
igorjsosares <igorjsoares@gmail.com>
2020-11-17 22:10:24 +0000

    Boas vindas 4

--- webhooks/webhook.php
+++ webhooks/webhook.php
@@ -206,7 +206,7 @@
         //* Função de LOG
         public function logSis($tipo, $texto)
         {
-            file_put_contents('log.txt', "> " . $tipo . " " . date('d/m/Y h:i:s') . $texto . PHP_EOL, FILE_APPEND);
+            file_put_contents('log.txt', "> " . $tipo . " " . date('d/m/Y h:i:s') . " " . $texto . PHP_EOL, FILE_APPEND);
         }
     } //# Class whatsAppBot
 
