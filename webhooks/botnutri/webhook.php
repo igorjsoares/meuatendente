f5ac71523131f6e0e798739ff6ca23d0a0d38af3
igorjsosares <igorjsoares@gmail.com>
2020-11-19 18:37:42 +0000

    Validação email 1

---
+++ webhooks/botnutri/webhook.php
@@ -0,0 +1,219 @@
+<?php {
+    class whatsAppBot
+    {
+
+        //$id_instancia = $_GET['codigo']; //chatpro-yybwcu3f69   
+
+        public function __construct()
+        {
+            include("../dados_conexao.php");
+
+            //& Alterar aqui depois os dados para a consulta no BD 
+            $tempoMenu = 7200; //Tempo entre a última mensagem e a possibilidade de enviar o menu novamente
+            $idInstancia = 1;
+            $this->idInstancia = $idInstancia;
+
+            //Recebe o corpo do Json enviado pela instância
+            $json = file_get_contents('php://input');
+            $decoded = json_decode($json, true); //Decodifica
+
+            //Grava o JSON-body no arquivo de debug
+            ob_start();
+            var_dump($decoded);
+            $input = ob_get_contents();
+            ob_end_clean();
+
+            //Coloca para salvar todas as requisições recebidas em um arquivo de log
+            //file_put_contents('inputs.log', $input . PHP_EOL, FILE_APPEND);
+
+
+
+            //() Verifica SE É uma mensagem recebida 
+            if (isset($decoded['Type']) && ($decoded['Type'] == 'receveid_message' || $decoded['Type'] == 'receveid_audio_message')) {
+                $RemoteJid = $decoded['Body']['Info']['RemoteJid'];
+                $RemoteJidArray = explode("@", $RemoteJid);
+                $numero = $RemoteJidArray[0];
+                $this->numerocliente = $numero;
+                $tipoNumero = $RemoteJidArray[1];
+                $idMensagemWhats = $decoded['Body']['Info']['Id'];
+                $timestamp = $decoded['Body']['Info']['Timestamp'];
+                $mensagem = $decoded['Body']['Text'];
+
+
+                //( Busca informações da instância CHATPRO no banco de dados 
+                $sql = "SELECT * FROM tbl_instancias WHERE id_instancia = $idInstancia";
+                $query = mysqli_query($conn['link'], $sql);
+                $consultaInstancia = mysqli_fetch_array($query, MYSQLI_ASSOC);
+                $numRow = mysqli_num_rows($query);
+                if (!$query) {
+                    echo "Erro ao tentar conectar no MYSQL " . mysqli_connect_error();
+                    $this->logSis('ERR', 'Mysql Connect: ' . mysqli_connect_error());
+
+                    exit(0);
+                }
+                if ($numRow == 0) { //VERIFICA SE EXISTE NO BANCO DE DADOS
+                    $this->logSis('ERR', "Instância N/E: " . $id_instancia);
+                    exit(0);
+                } else {
+                    $this->APIurl  = $consultaInstancia['endpoint'] . '/api/v1/';
+                    $this->token  = $consultaInstancia['token'];
+                    $this->numerosuporte =  $consultaInstancia['numero_suporte'];
+                    $this->conf_cad_dados =  $consultaInstancia['conf_cad_dados'];
+                    $this->msg_cad_dados =  $consultaInstancia['msg_cad_dados'];
+                    $this->msg_inicial =  $consultaInstancia['msg_inicial'];
+                    $limite = $consultaInstancia['limite'];
+                    $status = $consultaInstancia['status'];
+                    $nome = $consultaInstancia['nome'];
+                }
+
+
+                //() Verifica se NÃO É uma mensagem recebida de um número ou GRUPO 
+                if ($tipoNumero == 's.whatsapp.net') {
+
+                    //( Consulta o contato no BD 
+                    $sql = "SELECT * FROM tbl_contatos WHERE numero = $numero AND id_instancia = $idInstancia";
+                    $query = mysqli_query($conn['link'], $sql);
+                    $consultaContato = mysqli_fetch_array($query, MYSQLI_ASSOC);
+                    $numRow = mysqli_num_rows($query);
+
+                    if (!$query) {
+                        $this->logSis('ERR', "Mysql Connect Num: " . mysqli_connect_error());
+
+                        exit(0);
+                    }
+
+                    if ($numRow != 0) { //( O CONTATO EXISTE NO BANCO DE DADOS 
+
+                    } else { //( O CONTATO NÃO EXISTE 
+                        $this->primeirocontato = true;
+
+                        //CONTATO NÃO EXISTE 
+                        //( Insere o contato no banco de dados 
+                        $sql = "INSERT INTO tbl_contatos(id_instancia, numero, teste, created_at) VALUES ('$idInstancia', '$numero', 0, NOW())";
+                        $resultado = mysqli_query($conn['link'], $sql);
+                        $this->id_contato = mysqli_insert_id($conn['link']);
+                        if ($resultado != '1') {
+                            $this->logSis('ERR', 'Insert Contatos. Erro: ' . $resultado . mysqli_connect_error());
+                        }
+                    }
+
+                    //( Insere a interação que foi recebida no BD 
+                    //& Quando inserir a mensagem do cliente, já trazer o ID para colocar na coluna id_retorno na mensagem que vamos enviar. 
+                    //& Verificar também o retorno de erro, caso não consiga inserir o cliente. 
+                    $resultado = $this->inserirInteracao($this->idInstancia, 0, $this->id_contato, '', '', $idMensagemWhats, $mensagem, 1);
+
+                    if ($resultado == '1') {
+
+                        if ($this->primeirocontato == true) { //( Se for o primeiro contato
+                            //$this->ftcAbertura($decoded['Body']['Info']['RemoteJid'], true);
+
+                            $mensagem = explode(' ', trim($decoded['Body']['Text']));
+                            $palavra = mb_strtolower($mensagem[0], 'UTF-8');
+                            //( Verifica se o e-mail é valido
+                            $this->validaEmail($palavra, $numero);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //* PRIMEIRO CONTATO - Primeiras mensagens ou mensagem de erro 
+        public function primeiroContato($remoteJID, $primeiroContato)
+        {
+            if ($primeiroContato == true) {
+                if ($this->conf_cad_dados == 1) { //( Solicitar os dados ao cliente 
+                    $aberturaString = $this->msg_cad_dados;
+                } else {
+                    $aberturaString = $this->msg_inicial;
+                }
+                $this->sendMessage("Inicial", $remoteJID, $aberturaString);
+            }
+        }
+
+        public function validaEmail($palavra, $numero)
+        {
+            if (filter_var($palavra, FILTER_VALIDATE_EMAIL)) {
+                $validado = true;
+            } else {
+                $this->logSis('DEB', 'Email invalido. Número: ' . $numero . ' Enviado: ' . $palavra);
+                $validado = false;
+            }
+
+            if ($validado == true) { //E-mail válido
+                $this->sendMessage("Inicial", $numero, 'Esse é um e-mail válido');
+            } else { //e-mail invalido
+                $this->sendMessage("Inicial", $numero, "Que bom que você está aqui!\nPorém não identificamos um e-mal válido na mensagem enviada.\nPara receber nosso conteúdo, favor envie uma mensagem somente com o seu e-mail. ");
+            }
+        }
+
+        //* P R E P A R O  E N V I O
+        //Prepara para envio da mensagem de texto
+        public function sendMessage($motivo, $remoteJID, $text)
+        {
+            $data = array('number' => $remoteJID, 'menssage' => $text);
+            $this->sendRequest($motivo, 'send_message', $data);
+        }
+
+        //* E N V I O
+        //Envia a requisição
+        public function sendRequest($motivo, $method, $data)
+        {
+            include("../dados_conexao.php");
+
+            $url = 'https://' . $this->APIurl . $method;
+            if (is_array($data)) {
+                $data = json_encode($data);
+            }
+
+            $options = stream_context_create(['http' => [
+                'method'  => 'POST',
+                'header'  => "Content-type: application/json\r\nAuthorization: $this->token\r\n",
+                'content' => $data
+            ]]);
+
+            $response = file_get_contents($url, false, $options);
+
+            $this->logSis('REQ', 'Resp Requisição: ' . $response);
+
+            //return $response;
+
+            $resposta = json_decode($response, true);
+            $statusEnvio = $resposta['message'];
+            if ($statusEnvio == "Mensagem enviada com sucesso" || $statusEnvio == "Mensagem Enviada") {
+                $id_resposta = $resposta['requestMenssage']['id'];
+                $this->inserirInteracao($this->idInstancia, 1, $this->id_contato, 2, $this->id_interacao, $id_resposta, $motivo, 1);
+            } else {
+                $this->logSis('ERR', 'Não teve resposta da requisição a tempo' . $resposta);
+            }
+        } //# FCT Envio Requisição
+
+        public function inserirInteracao($id_instancia, $direcao, $id_contato, $tipo, $resposta, $id_mensagem, $mensagem, $status)
+        {
+            include("../dados_conexao.php");
+
+            $sql = "INSERT INTO tbl_interacoes(id_instancia, direcao, id_contato, tipo, resposta, id_mensagem, mensagem, status, data_envio) VALUES ($id_instancia, $direcao, $id_contato, '$tipo', '$resposta', '$id_mensagem', '$mensagem', $status, NOW())";
+            $resultado = mysqli_query($conn['link'], $sql);
+            $idInteracaoIn = mysqli_insert_id($conn['link']);
+            if ($resultado != '1') {
+                $erro = mysqli_error($conn['link']);
+                $this->logSis('ERR', 'Insert interação IN. Erro: ' . $erro);
+                $this->logSis('DEB', 'SQL : ' . $sql);
+            } else {
+                return 1;
+                $this->logSis('SUC', 'Insert interação IN. ID_Interação: ' . $idInteracaoIn);
+            }
+            mysqli_close($conn['link']);
+        }
+
+        //* Função de LOG
+        public function logSis($tipo, $texto)
+        {
+            file_put_contents('log.txt', "> " . $tipo . " " . date('d/m/Y h:i:s') . " " . $texto . PHP_EOL, FILE_APPEND);
+        }
+    } //# Class whatsAppBot
+
+
+    //executar a classe quando este arquivo for solicitado pela instância
+    new whatsAppBot();
+} //# Geral
