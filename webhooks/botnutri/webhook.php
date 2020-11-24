82157948a9bd5cc829b083f1b63aa47b0e443f63
igorjsosares <igorjsoares@gmail.com>
2020-11-24 21:33:13 +0000

    Resposta 23

--- webhooks/botnutri/webhook.php
+++ webhooks/botnutri/webhook.php
@@ -189,7 +189,7 @@
             }
 
             //(ULTIMA INTERAÇÃO DE MENU - O que provavelmente o cliente está respondendo 
-            $sql = "SELECT id_interacao, id_retorno FROM tbl_interacoes WHERE id_instancia = $this->idInstancia AND tipo = 1 AND direcao = 1 AND id_contato = $this->id_contato ORDER BY data_envio DESC LIMIT 1";
+            $sql = "SELECT id_interacao, id_retorno FROM tbl_interacoes WHERE modo = 1 AND id_instancia = $this->idInstancia AND tipo = 1 AND direcao = 1 AND id_contato = $this->id_contato ORDER BY data_envio DESC LIMIT 1";
             $query = mysqli_query($conn['link'], $sql);
             $numRow = mysqli_num_rows($query);
             $consultaUltima = mysqli_fetch_array($query, MYSQLI_ASSOC);
@@ -230,7 +230,7 @@
             include("dados_conexao.php");
 
             if ($id_retorno == '') { //ou seja, não sei qual o retorno
-                $sql = "SELECT * FROM tbl_retornos WHERE id_retorno = (SELECT resposta FROM tbl_opcoes WHERE modo = 1 AND id_instancia = $this->idInstancia AND indice = '$primeiraPalavraCliente' AND id_retorno = $ultimoRetorno)";
+                $sql = "SELECT * FROM tbl_retornos WHERE id_retorno = (SELECT resposta FROM tbl_opcoes WHERE id_instancia = $this->idInstancia AND indice = '$primeiraPalavraCliente' AND id_retorno = $ultimoRetorno)";
             } else { //Sei qual o retorno atual
                 //$idInstancia = $this->idInstancia;
                 $sql = "SELECT * FROM tbl_retornos WHERE id_instancia = $this->idInstancia AND id_retorno = $id_retorno";
