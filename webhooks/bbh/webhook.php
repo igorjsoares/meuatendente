678d77f244f1cc89380629d19dfa6036f1411515
igorjsosares <igorjsoares@gmail.com>
2020-12-09 22:15:13 +0000

    Resposta Opções Variáveis 25

--- webhooks/bbh/webhook.php
+++ webhooks/bbh/webhook.php
@@ -586,7 +586,7 @@
                 if ($result == false) {
                     //& Problema, o que retornar ao usuário???
                 } else {
-                    $result = $result[0];
+                    //$result = $result[0];
                     foreach ($result as $linha) {
                         array_push($arrayOpcoes, array('indice' => 3, 'tipo' => $filtroTipo, 'id' => $linha['id'], 'nome' => $linha['nome'], 'descricao' => $linha['descricao'], 'tamanho' => $linha['tamanho'], 'valor' => $linha['valor'], 'valor_promo' => $linha['valor_promo'], 'ofertas' => $linha['ofertas']));
                     }
