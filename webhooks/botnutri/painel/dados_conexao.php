89b92cc78adacd225f83c33570334b1bafbd8691
igorjsosares <igorjsoares@gmail.com>
2021-01-07 14:30:56 +0000

    Painel 3

--- webhooks/botnutri/painel/dados_conexao.php
+++ webhooks/botnutri/painel/dados_conexao.php
@@ -8,19 +8,16 @@ $conn['abreSessao'] = true;         // Inicia a sess�o com um session_start()?
 $conn['validaSempre'] = true;       // Deseja validar o usu�rio e a senha a cada carregamento de p�gina?
 // Evita que, ao mudar os dados do usu�rio no banco de dados o mesmo contiue logado.
 
-$conn['servidor'] = 'mysql669.umbler.com:41890';    // Servidor MySQL
-$conn['usuario'] = 'prospect_admin';          // Usu�rio MySQL
+$conn['servidor'] = 'mysql741.umbler.com';    // Servidor MySQL
+$conn['usuario'] = 'botnutri_admin';          // Usu�rio MySQL
 $conn['senha'] = 'ROGImari1518';                // Senha MySQL
-$conn['banco'] = 'newprospect';    			// Banco de dados MySQL
-
-//Token API MessageFlow
-$token_messageflow = 'Token c3adcffb-122f-4a71-9fae-b872bb0a6b67';
+$conn['banco'] = 'botnutri';    			// Banco de dados MySQL
 
 // ==============================
 
 // Verifica se precisa fazer a conex�o com o MySQL
 if ($conn['conectaServidor'] == true) {
-$conn['link'] = mysqli_connect($conn['servidor'], $conn['usuario'], $conn['senha']) or die("MySQL: Não foi possível conectar-se ao servidor [".$conn['servidor']."].");
+$conn['link'] = mysqli_connect($conn['servidor'], $conn['usuario'], $conn['senha']) or die("MySQL: N�o foi poss�vel conectar-se ao servidor [".$conn['servidor']."].");
 mysqli_select_db($conn['link'], $conn['banco']) or die("MySQL: Não foi possível conectar-se ao banco de dados [".$conn['banco']."].");
 }
 
--- webhooks/botnutri/painel/js/app.js
+++ webhooks/botnutri/painel/js/app.js
@@ -1,13 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDt1Wf8_73mEo-oFyArmENkhm7UUcquNeY",
-    authDomain: "newprospect-129bd.firebaseapp.com",
-    databaseURL: "https://newprospect-129bd.firebaseio.com",
-    projectId: "newprospect-129bd",
-    storageBucket: "newprospect-129bd.appspot.com",
-    messagingSenderId: "453499985757",
-    appId: "1:453499985757:web:7cdfa8b7b8d049a891d2b4",
-    measurementId: "G-QY12BNCHZY"
+  apiKey: "AIzaSyDW1wDrcNRQtRyWX-r8aCj4yXsJXZl0r8Q",
+  authDomain: "nutri-3de11.firebaseapp.com",
+  projectId: "nutri-3de11",
+  storageBucket: "nutri-3de11.appspot.com",
+  messagingSenderId: "529959667959",
+  appId: "1:529959667959:web:3384721b19c30ff20fabf2",
+  measurementId: "G-F9D0J50Z7D"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
