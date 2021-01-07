976c04ad997270f83a33efbec5b4af038b787960
igorjsosares <igorjsoares@gmail.com>
2021-01-07 17:52:06 +0000

    Atendimento 16

--- webhooks/botnutri/painel/atendimento.php
+++ webhooks/botnutri/painel/atendimento.php
@@ -71,41 +71,111 @@ $aleatorio = rand(1000, 10000);
                                     </ul>
                                 </div>
                                 <div class="col-8 col-md-8">
-                                    <h1>Calcule seu consumo mínimo de água</h1>
-                                    <h5 style="margin-bottom: 40px">Nutri Mari Martins</h5>
-                                    <div id="divForm">
-                                        <div class="row">
-                                            <div class="form-group" style="margin-left: 15px">
-                                                <label for="peso">Seu peso em kg</label>
-                                                <input style="width: 116px" class="form-control" type="number" id="peso" />
+                                    <!-- DIRECT CHAT PRIMARY -->
+                                    <div class="card card-primary card-outline direct-chat direct-chat-primary">
+                                        <div class="card-header">
+                                            <h3 class="card-title">Direct Chat</h3>
+
+                                            <div class="card-tools">
+                                                <span title="3 New Messages" class="badge bg-primary">3</span>
+                                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                                    <i class="fas fa-minus"></i>
+                                                </button>
+                                                <button type="button" class="btn btn-tool" title="Contacts" data-widget="chat-pane-toggle">
+                                                    <i class="fas fa-comments"></i>
+                                                </button>
+                                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                                    <i class="fas fa-times"></i>
+                                                </button>
                                             </div>
-                                            <button id="botao" class="btn btn-primary btn-sm" style="margin-left: 20px; margin: 15px">Quanto de água preciso beber?</button>
                                         </div>
-                                    </div>
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <!-- Conversations are loaded here -->
+                                            <div class="direct-chat-messages">
+                                                <!-- Message. Default to the left -->
+                                                <div class="direct-chat-msg">
+                                                    <div class="direct-chat-infos clearfix">
+                                                        <span class="direct-chat-name float-left">Alexander Pierce</span>
+                                                        <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
+                                                    </div>
+                                                    <!-- /.direct-chat-infos -->
+                                                    <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image">
+                                                    <!-- /.direct-chat-img -->
+                                                    <div class="direct-chat-text">
+                                                        Is this template really for free? That's unbelievable!
+                                                    </div>
+                                                    <!-- /.direct-chat-text -->
+                                                </div>
+                                                <!-- /.direct-chat-msg -->
 
+                                                <!-- Message to the right -->
+                                                <div class="direct-chat-msg right">
+                                                    <div class="direct-chat-infos clearfix">
+                                                        <span class="direct-chat-name float-right">Sarah Bullock</span>
+                                                        <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
+                                                    </div>
+                                                    <!-- /.direct-chat-infos -->
+                                                    <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image">
+                                                    <!-- /.direct-chat-img -->
+                                                    <div class="direct-chat-text">
+                                                        You better believe it!
+                                                    </div>
+                                                    <!-- /.direct-chat-text -->
+                                                </div>
+                                                <!-- /.direct-chat-msg -->
+                                            </div>
+                                            <!--/.direct-chat-messages-->
 
-                                    <div id="resultado" class="jumbotron" style="margin: 20px; display: none; padding: 2rem 2rem">
-                                        <div class="container">
-                                            <h2 id="titulo" class="display-4"></h2>
+                                            <!-- Contacts are loaded here -->
+                                            <div class="direct-chat-contacts">
+                                                <ul class="contacts-list">
+                                                    <li>
+                                                        <a href="#">
+                                                            <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg" alt="User Avatar">
+
+                                                            <div class="contacts-list-info">
+                                                                <span class="contacts-list-name">
+                                                                    Count Dracula
+                                                                    <small class="contacts-list-date float-right">2/28/2015</small>
+                                                                </span>
+                                                                <span class="contacts-list-msg">How have you been? I was...</span>
+                                                            </div>
+                                                            <!-- /.contacts-list-info -->
+                                                        </a>
+                                                    </li>
+                                                    <!-- End Contact Item -->
+                                                </ul>
+                                                <!-- /.contatcts-list -->
+                                            </div>
+                                            <!-- /.direct-chat-pane -->
                                         </div>
+                                        <!-- /.card-body -->
+                                        <div class="card-footer">
+                                            <form action="#" method="post">
+                                                <div class="input-group">
+                                                    <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                                                    <span class="input-group-append">
+                                                        <button type="submit" class="btn btn-primary">Send</button>
+                                                    </span>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <!-- /.card-footer-->
                                     </div>
-                                    <button id="botaoNovamente" style="color: #B1B7C6; margin-top: 10px; display: none; margin-left: 15px" class="btn btn-outline-light btn-sm">Calcular novamente</button>
-                                    <div style="padding: 15px">
-                                        <p style="color: #B1B7C6; line-height: initial;"><small>
-                                                Quando falamos que todos devem beber 2 litros de água por dia, isso é uma estimativa.
-                                                A quantidade exata de água que cada pessoa necessita ingerir diariamente varia de acordo com o peso, idade, estação do ano (durante tempos de calor perdemos mais líquido com a sudorese) e outros fatores, como prática de atividade física (deve aumentar ingestão de água).
-                                            </small></p>
-                                    </div>
+                                    <!--/.direct-chat -->
                                 </div>
+                                <!-- /.col -->
                             </div>
                         </div>
-
                     </div>
 
                 </div>
+
             </div>
-            <!-- /.card -->
         </div>
+        <!-- /.card -->
+    </div>
     </div>
     </div>
 </section>
