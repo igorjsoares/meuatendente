4a0fa41e5d785f7dc001376b5bdb79053007e9fb
igorjsosares <igorjsoares@gmail.com>
2021-01-09 15:47:51 +0000

    Implantação do temporizador e atualizações periódias 1

--- webhooks/botnutri/painel/js/atendimento.js
+++ webhooks/botnutri/painel/js/atendimento.js
@@ -7,7 +7,7 @@ $(function () {
     console.log("Iniciando atendimento.js")
 
     window.previsao = moment()
-    window.tempoAtualizacao = 1000 * 10 //x*y onde y representa os segundos
+    window.tempoAtualizacao = 1000 * 30 //x*y onde y representa os segundos
 
     window.intervalo = window.setInterval(atualizacaoPeriodica, window.tempoAtualizacao)
 
@@ -53,74 +53,11 @@ $(function () {
 
             console.log(content)
 
-            /*
- 
-             $conteudo = ''
-             for (var i = 0; i < content.length; i++) {
-                 switch (content[i]['status']) {
-                     case 'processing':
-                         var textoStatus = 'Processando'
-                         var corStatus = 'light'
-                         break;
-                     case 'waiting':
-                         var textoStatus = 'Aguardando'
-                         var corStatus = 'light'
-                         break;
-                     case 'running':
-                         var textoStatus = 'Rodando'
-                         var corStatus = 'info'
-                         break;
-                     case 'finished':
-                         var textoStatus = 'Finalizada'
-                         var corStatus = 'success'
-                         break;
-                     case 'closed':
-                         var textoStatus = 'Fechada'
-                         var corStatus = 'secondary'
-                         break;
-                     case 'error':
-                         var textoStatus = 'Erro'
-                         var corStatus = 'danger'
-                         break;
-                     case 'validating':
-                         var textoStatus = 'Validando'
-                         var corStatus = 'warning'
-                         break;
- 
-                     default:
-                         break;
-                 }
-                 moment.locale('pt-br')
-                 var inicio = moment(content[i]['running_date']).format('DD/MM/YYYY H:mm')
-                 if (inicio == 'Invalid date') {
-                     inicio = "";
-                 }
-                 var fim = moment(content[i]['finished_date']).format('DD/MM/YYYY H:mm')
-                 if (fim == 'Invalid date') {
-                     fim = "";
-                 }
-                 var totalContatos = parseInt(content[i]['delivered']) + parseInt(content[i]['inactive_whatsapp']) + parseInt(content[i]['read']) + parseInt(content[i]['unverified'])
-                 var idConta = content[i]['id_conta']
-                 var nomeConta = content[i]['nome_conta']
- 
-                 $conteudo += '<tr><td>' + content[i]['idMessageFlow'] + '</td>'
-                 $conteudo += '<td>' + content[i]['nome'] + '</td>'
-                 $conteudo += '<td>' + totalContatos + '</td>'
-                 $conteudo += '<td>' + inicio + '</td>'
-                 $conteudo += '<td>' + fim + '</td>'
-                 $conteudo += '<td><span class="badge badge-' + corStatus + '">' + textoStatus + '</span></td>'
-                 $conteudo += '<td>'
-                 $conteudo += '<a style="margin: 2px" href="#" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modalPreview" data-idcampanha="' + content[i]['idMessageFlow'] + '" data-nomecampanha="' + content[i]['nome'] + '" data-nomelista="' + content[i]['nome_lista'] + '" data-idconta="' + idConta + '" data-nomeconta="' + nomeConta + '" data-totalcontatos="' + totalContatos + '" data-totalcontatos="' + totalContatos + '" data-inicio="' + inicio + '" data-fim="' + fim + '" data-textostatus="' + textoStatus + '" data-corstatus="' + corStatus + '" data-lidos="' + parseInt(content[i]['read']) + '" data-entregues="' + parseInt(content[i]['delivered']) + '" data-inativos="' + parseInt(content[i]['inactive_whatsapp']) + '" data-naoverificados="' + parseInt(content[i]['unverified']) + '" data-tipo="' + content[i]['tipo'] + '" data-mensagem="' + content[i]['mensagem'] + '" data-url="' + content[i]['urlArquivo'] + '"><i class="fas fa-mobile-alt"></i></a>'
-                 $conteudo += '<a style="margin: 2px" href="#" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modalRelatorio" data-idcampanha="' + content[i]['idMessageFlow'] + '" data-nomecampanha="' + content[i]['nome'] + '" data-nomelista="' + content[i]['nome_lista'] + '" data-totalcontatos="' + totalContatos + '" data-inicio="' + inicio + '" data-fim="' + fim + '" data-textostatus="' + textoStatus + '" data-corstatus="' + corStatus + '" data-lidos="' + parseInt(content[i]['read']) + '" data-entregues="' + parseInt(content[i]['delivered']) + '" data-inativos="' + parseInt(content[i]['inactive_whatsapp']) + '" data-naoverificados="' + parseInt(content[i]['unverified']) + '"><i class="fas fa-chart-pie"></i></a>'
-                 //$conteudo += '<a style="margin: 2px" href="https://wa.me/" class="btn btn-info btn-sm"><i class="far fa-copy"></i></a>'
-                 //$conteudo += '<a style="margin: 2px" href="https://wa.me/" class="btn btn-info btn-sm"><i class="fas fa-pencil-alt"></i></a>'
-                 //$conteudo += '<a style="margin: 2px" href="" class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></a></td>'
-                 $conteudo += "</tr>"
-                }
- */
             var conteudo = ''
 
             for (var i = 0; i < content.length; i++) {
+                window.ultimaRecebida = consultaUltimaRecebida()
+                console.log('Return da Ultima recebida: ' + window.ultimaRecebida)
                 if (content[i]['nome'] != '') {
                     var nome = content[i]['nome']
                 } else {
@@ -152,6 +89,31 @@ $(function () {
         }
     })
 })
+
+function consultaUltimaRecebida() {
+    $.ajax({
+        url: 'ajaxs/atendimentoAjax.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            acao: 'consultaUltimaRecebida',
+            dados: {
+            }
+        },
+        beforeSend: function () {
+            console.log('Consultando ultima recebida')
+        },
+        success: function (content) {
+            console.log('Consultado ultima recebida')
+
+            console.log(content)
+
+            var conteudo = ''
+
+            return content[0]['ultima_recebida']
+        }
+    })
+}
 
 //* FUNÇÃO Seleção da conversa
 function fctClickMenu(idContato, nome, quant) {
@@ -193,7 +155,7 @@ function fctClickMenu(idContato, nome, quant) {
                     conteudo += '<div class="direct-chat-text" style="margin-left: 0px; margin-right: 0px; width: 100%; background-color: #FFF;">'
                     conteudo += content[i]['mensagem']
                     conteudo += '</div>'
-                    conteudo += '<div style="color: #C1C1C1; font-size: 8px; text-align: left; margin-left: 10px">'+content[i]['dataEnvio']+'</div>'
+                    conteudo += '<div style="color: #C1C1C1; font-size: 8px; text-align: left; margin-left: 10px">' + content[i]['dataEnvio'] + '</div>'
                     conteudo += '</div>'
 
                 } else { //enviada    
@@ -202,7 +164,7 @@ function fctClickMenu(idContato, nome, quant) {
                     conteudo += '<div class="direct-chat-text" style="margin-left: 0px; margin-right: 0px; width: 100%; background-color: #DBF7C6;">'
                     conteudo += content[i]['mensagem']
                     conteudo += '</div>'
-                    conteudo += '<div style="color: #C1C1C1; font-size: 8px; text-align: right; margin-right: 10px">'+content[i]['dataEnvio']+'</div>'
+                    conteudo += '<div style="color: #C1C1C1; font-size: 8px; text-align: right; margin-right: 10px">' + content[i]['dataEnvio'] + '</div>'
                     conteudo += '</div>'
                 }
 
@@ -254,7 +216,66 @@ function alterarStatusChat(idContato) {
 function atualizacaoPeriodica() {
     clearInterval(window.tempoAtualizacao)
     console.log("Atualização periódica")
+
 }
+
+//* FUNÇÃO de consulta do Menu
+function consultaMenu() {
+    $.ajax({
+        url: 'ajaxs/atendimentoAjax.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            acao: 'consultaMenuAtendimento',
+            dados: {
+            }
+        },
+        beforeSend: function () {
+            console.log('Consultando menu periódico de atendimento')
+        },
+        success: function (content) {
+            console.log('Consultado menu periódico')
+
+            //$("#overlayTabela").add('hidden')
+            //$("#overlayTabela").removeClass('d-flex')
+
+            console.log(content)
+
+            var conteudo = ''
+
+            for (var i = 0; i < content.length; i++) {
+                if (content[i]['nome'] != '') {
+                    var nome = content[i]['nome']
+                } else {
+                    var nome = content[i]['numero']
+                }
+                var nomeComAspas = "'" + nome + "'"
+                conteudo += '<li class="nav-item" onclick="fctClickMenu(' + content[i]['idContato'] + ', ' + nomeComAspas + ', ' + content[i]['quant'] + ')" style="cursor:pointer">'
+                conteudo += '<div style="padding: 10px" class="row align-items-center">'
+                conteudo += '<div class="col-2">'
+                conteudo += '<div style="padding: 0px;" class="image">'
+                conteudo += '<img style="width: 45px; height: 45px" id="imgEmpresaMenu" src="assets/empresas/avatar.png" class="img-circle elevation-2" alt="">'
+                conteudo += '</div>'
+                conteudo += '</div>'
+                conteudo += '<div class="col-8">'
+                conteudo += '<font style="font-size: 20px;">' + nome + '</font><br>'
+                conteudo += '<font style="font-size: 13px; color: gray">Mensagem enviada</font>'
+                conteudo += '</div>'
+                conteudo += '<div class="col-2">'
+                if (content[i]['quant'] > 0) {
+                    conteudo += '<span class="float-right badge bg-success" id="span' + content[i]['idContato'] + '">' + content[i]['quant'] + '</span>'
+                }
+                conteudo += '</div>'
+                conteudo += '</div>'
+                conteudo += '</li>'
+                conteudo += '</div>'
+            }
+
+            document.getElementById('ulMenuConversas').innerHTML += conteudo
+        }
+    })
+}
+
 
 //* FUNÇÃO de notificação
 function notify(alert, alert_message) {
