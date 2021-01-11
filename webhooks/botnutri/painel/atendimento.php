63c49d701dbe723d57c0029cffa94a45900d882f
igorjsosares <igorjsoares@gmail.com>
2021-01-11 08:21:50 +0000

    Atualização parcial de menu 12

--- webhooks/botnutri/painel/atendimento.php
+++ webhooks/botnutri/painel/atendimento.php
@@ -642,4 +642,6 @@ $aleatorio = rand(1000, 10000);
 <!-- Select2 -->
 <script src="plugins/select2/js/select2.full.min.js"></script>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+
 <script src="js/atendimento.js"></script>
