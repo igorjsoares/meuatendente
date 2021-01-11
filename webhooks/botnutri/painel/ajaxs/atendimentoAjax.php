7e849872628e2761deaf758389b10be14b0f43f9
igorjsosares <igorjsoares@gmail.com>
2021-01-11 20:32:16 +0000

    Preview códigos 10

--- webhooks/botnutri/painel/ajaxs/atendimentoAjax.php
+++ webhooks/botnutri/painel/ajaxs/atendimentoAjax.php
@@ -111,8 +111,8 @@ switch ($acao) {
         $dados = $_POST['dados'];
         $idRetorno = filter_var($dados['idRetorno'], FILTER_SANITIZE_STRING);
 
-        $sql = "SELECT id_retorno, mensagem FROM tbl_retornos WHERE id_retorno = $idRetorno";
-        logSis('DEB', 'SQL : ' . $sql.' Result: '.print_r(mysqli_fetch_array($query), true));
+        $sql = "SELECT id_retorno, mensagem FROM tbl_retornos WHERE id_retorno = $idRetorno AND id_retorno = 10";
+        //logSis('DEB', 'SQL : ' . $sql.' Result: '.print_r(mysqli_fetch_array($query), true));
 
         $query = mysqli_query($conn['link'], $sql);
 
