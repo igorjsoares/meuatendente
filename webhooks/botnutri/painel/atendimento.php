8d127e565e0a82987ae687fc5ead4c13d1a048c7
igorjsosares <igorjsoares@gmail.com>
2021-01-11 09:06:17 +0000

    Atualização parcial de menu 32

--- webhooks/botnutri/painel/atendimento.php
+++ webhooks/botnutri/painel/atendimento.php
@@ -109,6 +109,7 @@ $aleatorio = rand(1000, 10000);
                         <li value="2021-01-11 05:22:27">doesntmatter2</li>
                         <li value="2021-01-07 19:39:31">doesntmatter3</li>
                     </ul> -->
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
                     <ul id="list">
                         <li value="2021-01-11 05:36:36">doesnmatter1</li>
                         <li value="2021-01-11 05:22:27">doesntmatter2</li>
